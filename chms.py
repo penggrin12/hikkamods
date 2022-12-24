22c61692bf349656dd012753d6da634f483f6189
penggrin12 <76795163+penggrin12@users.noreply.github.com>
2022-12-24 23:21:35 +0300

    CHMS Client 1.0

--- chms.py
+++ chms.py
@@ -2,14 +2,12 @@
 # Copyright (c) 2022 penggrin
 
 # meta developer: @penggrinmods
-# scope: hikka_only
 # scope: hikka_we
 # scope: hikka_min 1.2.10
 
 import asyncio
 import logging
 
-from telethon.tl.types import Message
 from .. import loader, main, translations, utils
 
 logger = logging.getLogger(__name__)
@@ -19,22 +17,50 @@ logger = logging.getLogger(__name__)
 class CHMSMod(loader.Module):
     """Load Hikka modules with "Install Via CHMS"."""
 
-    strings = {"name": "CHMS"}
+    strings = {
+        "name": "CHMS",
+        "warning": (
+            "⚠️ Looks like you added un-offical sources to your trusted sources. Note that this <b>can be dangerous!</b>"
+            " (You can disable this warning in <code>.config</code>)"
+        ),
+        "cfg_trusted": "Trusted sources to load modules from (ID or @USERNAME without the @)",
+        "cfg_no_warnings": "Do not show warnings at startup"
+    }
+
+    strings_ru = {
+        "warning": (
+            "⚠️ Вы добавили не-официального бота в свой список доверенных ботов. Знайте что это действие <b>может быть опасным!</b>"
+            " (Вы можете выключить это предупреждение в <code>.config</code>)"
+        ),
+        "cfg_trusted": "Доверенные боты из которых можно загружать модули (АЙДИ или @ЮЗЕРНЕЙМ без @)",
+        "cfg_no_warnings": "Не показывать предупреждения при запуске"
+    }
 
     def __init__(self):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "trusted",
-                [5829488433],
-                "Trusted sources to load modules from",
-                validator=loader.validators.Series(loader.validators.Integer()),
+                [5829488433], # official sources
+                lambda: self.config("cfg_trusted"),
+                validator=loader.validators.Series()
+            ),
+            loader.ConfigValue(
+                "no_warnings",
+                False,
+                lambda: self.config("cfg_no_warnings"),
+                validator=loader.validators.Boolean()
             )
         )
 
     async def client_ready(self, *_):
         for source in self.config["trusted"]:
             await utils.dnd(self._client, source, archive=False)
+
         self.loader_m = self.lookup("loader")
+        self.version = "1.0"
+
+        if (self.config != [5829488433]) and (not self.config["no_warnings"]):
+            await self.client.send_message("me", self.strings("warning"))
 
     async def _load_module(self, message):
         doc = (await message.download_media(bytes)).decode("utf-8")
@@ -46,13 +72,22 @@ class CHMSMod(loader.Module):
         logger.debug("Done loading new custom module!")
         await message.reply(f"#done")
 
+    @loader.command(ru_doc="Показать версию Клиента CHMS")
+    async def chmsversion(self, message):
+        """Show CHMS Client version"""
+        await utils.answer(message, self.version)
+
     @loader.watcher(only_docs=True)
-    async def watcher(self, message: Message):
+    async def watcher(self, message):
         if not isinstance(message, Message):
             return
+        if not (message.sender_id in self.config["trusted"]):
+            return
 
-        if (message.sender_id in self.config["trusted"]) and (message.raw_text.startswith("#cinstall")):
-            await message.delete()
+        await message.delete()
+
+        if message.raw_text == "#version":
+            await message.reply(self.version)
+        elif message.raw_text == "#cinstall":
             logger.debug("Installing a module from a trusted source")
-
             await self._load_module(message)
