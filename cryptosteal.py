7e9eb166008db5ba44ac86fa96fee3df2edb9197
Penggrin <miner.sidor@gmail.com>
2023-04-09 21:04:53 +0300

    Anti-SSCA & Delay before activating & Link to the check message

--- cryptosteal.py
+++ cryptosteal.py
@@ -4,16 +4,19 @@
 # meta developer: @PenggrinModules
 # scope: hikka_only
 
+# https://t.me/antissca
+
 from .. import loader, utils
 import logging
+import asyncio
 
-__version__ = (1, 2, 0)
+__version__ = (1, 3, 0)
 logger = logging.getLogger(__name__)
 
 
 @loader.tds
 class CryptoStealMod(loader.Module):
-    """Automatically claims cryptobot (and some other bots) checks, special thanks to @toxicuse"""
+    """Automatically claims cryptobot (and some other bots) checks, t.me/antissca. special thanks to @toxicuse"""
 
     strings = {
         "name": "CryptoSteal",
@@ -21,11 +24,20 @@ class CryptoStealMod(loader.Module):
         "enabled": "<emoji document_id=5206607081334906820>✅</emoji> Enabled",
         "status_now": "<emoji document_id=5449687343931859785>🤑</emoji> Crypto-Steal was <b>{}</b>!",
         "config_status": "Are we ready to steal?",
+        "config_delay": (
+            "How long to wait before check activation? (in seconds) (needed to prevent"
+            " moments when cryptobot didnt create the check yet)"
+        ),
+        "config_antissca_status": "Ignore checks by the members of SSCA?",
         "config_allow_other_bots": "If disabled i will only steal checks by Trusted Bots",
         "config_use_asset_chat": "If disabled the 'crypto-steal' will not be used",
         "config_trusted_bots": "Trusted Bots to steal from even if allow_other_bots is False (lowercase username)",
-        "cant_create_asset_chat": "The asset chat is not created, for some reason.",
-        "asset_chat_got_check": "☘️ Hopefully got a new check, here is the link to it: {u1}?start={u2}\nor:\n<code>/start {u2}</code> in {u1}",
+        "cant_create_asset_chat": "😢 The asset chat is not created, for some reason.",
+        "asset_chat_got_check": (
+            "☘️ Hopefully got a new check!\n🔗 Here is the link to it: {u1}?start={u2} or <code>/start {u2}</code> in {u1}"
+            '\n\n<a href="{link}">🔗 Message</a>'
+        ),
+        "got_ssca_check": "Got a check by an SSCA member!\n🔗 Here is the link to that message: {}"
     }
 
     strings_ru = {
@@ -33,20 +45,41 @@ class CryptoStealMod(loader.Module):
         "enabled": "<emoji document_id=5206607081334906820>✅</emoji> Включён",
         "status_now": "<emoji document_id=5449687343931859785>🤑</emoji> Crypto-Steal теперь <b>{}</b>!",
         "config_status": "Готовы ли мы тырить?",
+        "config_delay": "Сколько секунд ждать перед активацией чека? (нужно чтобы не было таким случаев когда криптобот ещё не создал чек)",
+        "config_antissca_status": "Игнорировать чеки от участников SSCA?",
         "config_allow_other_bots": "Если выключено то я буду тырить только чеки Доверенных Ботов",
         "config_use_asset_chat": "Если выключено то чат 'crypto-steal' не будет использован",
         "config_trusted_bots": "Доверенные Боты из которых я буду тырить даже если allow_other_bots на False (ник маленькими буквами)",
-        "cant_create_asset_chat": "Не удалось создать чат Crypto-Steal, почему-то.",
-        "asset_chat_got_check": "☘️ Надеюсь получил новый чек, вот ссылка на него: {u1}?start={u2}\nили:\n<code>/start {u2}</code> в {u1}",
+        "cant_create_asset_chat": "😢 Не удалось создать чат Crypto-Steal, почему-то.",
+        "asset_chat_got_check": (
+            "☘️ Надеюсь получил новый чек!\n🔗 Вот ссылка на него: {u1}?start={u2} или <code>/start {u2}</code> в {u1}"
+            '\n\n<a href="{link}">🔗 Сообщение</a>'
+        ),
+        "got_ssca_check": "Нашёл чек от участника SSCA!\n🔗 Вот ссылка на это сообщение: {}"
     }
+
+    async def get_ssca_members(self) -> str:
+        return (await self.client.get_messages("antissca", ids=[3]))[0].raw_text
 
     def __init__(self):
         self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "antissca_status",
+                True,
+                lambda: self.strings("config_antissca_status"),
+                validator=loader.validators.Boolean()
+            ),
             loader.ConfigValue(
                 "status",
                 True,
                 lambda: self.strings("config_status"),
                 validator=loader.validators.Boolean()
+            ),
+            loader.ConfigValue(
+                "delay",
+                0.08,
+                lambda: self.strings("config_delay"),
+                validator=loader.validators.Float()
             ),
             loader.ConfigValue(
                 "allow_other_bots",
@@ -87,6 +120,11 @@ class CryptoStealMod(loader.Module):
             await self.inline.bot.send_message(self._client.tg_id, self.strings("cant_create_asset_chat"))
             logger.error("cant create asset chat")
 
+    @loader.command(ru_doc="Отображает список участников SSCA")
+    async def sscateam(self, message):
+        """Lists SSCA members"""
+        return await utils.answer(message, await self.get_ssca_members())
+
     @loader.watcher(only_messages=True, only_inline=True)
     async def watcher(self, message):
         text = message.raw_text.lower()
@@ -105,19 +143,37 @@ class CryptoStealMod(loader.Module):
 
         user = await self.client.get_entity(url[0])
 
+        if str(message.from_id) in await self.get_ssca_members():
+            return await self.inline.bot.send_message(
+                f"-100{self.asset_chat[0].id}",
+                self.strings("got_ssca_check").format(
+                    f"https://t.me/c/{str(message.chat_id).replace('-100', '')}/{message.id}"
+                ),
+                disable_web_page_preview=True
+            )
+
         if (user.username.lower() not in self.config["trusted_bots"]) and (not self.config["allow_other_bots"]):
-            logger.debug(f"Ignoring not trusted bot (@{user.username})")
-            return
+            return logger.debug(f"Ignoring not trusted bot (@{user.username})")
+
+        # https://t.me/c/1955174868/656
+        await message.mark_read()
+
+        await asyncio.sleep(self.config["delay"])
 
         await self.client.send_message(user.id, f"/start {url[1]}")
-        already_claimed.append(url[1])
         logger.debug("Sent check get request, hopefully we got it")
+
+        already_claimed.append(url[1])
         self.db.set(__name__, 'already_claimed', already_claimed)
 
         if self.asset_chat and self.config["use_asset_chat"]:
             await self.inline.bot.send_message(
                 f"-100{self.asset_chat[0].id}",
-                self.strings("asset_chat_got_check").format(u1=url[0], u2=url[1]),
+                self.strings("asset_chat_got_check").format(
+                    u1=url[0],
+                    u2=url[1],
+                    link=f"https://t.me/c/{str(message.chat_id).replace('-100', '')}/{message.id}"
+                ),
                 disable_web_page_preview=True
             )
 
